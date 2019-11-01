5f7db0b73c26d39a0b7ac3169ba47d5110a43ced
wangbo67 <wangbokb@163.com>
2019-11-01 15:05:13 +0800

    Java并发编程实践-6."等待-通知"机制优化循环等待

---
+++ src/test/java/com/dcoder/Test06.java
@@ -0,0 +1,60 @@
+package com.dcoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * "等待-通知"机制优化循环等待
+ */
+public class Test06 {
+    private List<Object> als = new ArrayList<>();
+
+    class Allocator {
+        //一次性申请所有资源
+        synchronized void apply(Object from, Object to) {
+            //经典写法
+            while (als.contains(from) || als.contains(to)) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            als.add(from);
+            als.add(to);
+        }
+
+        synchronized void free(Object from, Object to) {
+            als.remove(from);
+            als.remove(to);
+
+            notifyAll();
+        }
+    }
+
+    class Account {
+        //als 必须为单例
+        private Allocator als;
+        private int balance;
+
+        //转账
+        void transfer(Account target, int amt) {
+            try {
+                als.apply(this, target);
+                //锁定转出账户
+                synchronized (this) {
+                    //锁定转入账户
+                    synchronized (target) {
+                        if(this.balance > amt) {
+                            this.balance -= amt;
+                            target.balance += amt;
+                        }
+                    }
+                }
+            } finally {
+                als.free(this, target);
+            }
+        }
+    }
+}
